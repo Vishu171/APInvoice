7f5a74fa753528c9dd57ae2bae196c7a01ceb8ac
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2024-07-09 23:03:23 +0530

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -10,7 +10,7 @@ from langchain.llms import OpenAI
 
 
 
-LETTER_TEMPLATE = """ Your Objective is to give detailed information from invoices considering the users question.
+LETTER_TEMPLATE = """ Your Objective is to give detailed information from invoices considering the users question and give the answer that is specific to the users question.
 
 Context: ```{context}```
 
@@ -46,6 +46,6 @@ def letter_chain(question):
                                             retriever=retreiver,
                                            chain_type="stuff", #"stuff", "map_reduce","refine", "map_rerank"
                                            return_source_documents=True,
-                                           #chain_type_kwargs={"prompt": LETTER_PROMPT}
+                                           chain_type_kwargs={"prompt": LETTER_PROMPT}
                                           )
     return qa_chain({"query": question})
